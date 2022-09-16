7523b02c6124b56a1c830b8ef45c3cbb0ab07cf2
Olle Reberg <olle.reberg@student.forsbergsskola.se>
2022-09-16 11:42:49 +0200

    Starting to convert blueprints to C++

---
+++ Plugins/InventoryPlugin/Source/InventoryPlugin/Public/InventoryStructs.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "ItemPDABase.h"
+#include "InventoryStructs.generated.h"
+
+USTRUCT()
+struct FItemStruct
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	UItemPDABase* ItemPDA;
+
+	UPROPERTY()
+	float Durability;
+};
---
+++ Plugins/InventoryPlugin/Source/InventoryPlugin/Public/ItemPDABase.h
@@ -0,0 +1,14 @@
+﻿
+
+#pragma once
+#include "Engine/DataAsset.h"
+#include "ItemPDABase.generated.h"
+
+
+
+UCLASS()
+class INVENTORY_API UItemPDABase : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+};
